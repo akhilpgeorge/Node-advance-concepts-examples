3be237e20f1bf62329779463f885bff2c5195642
Akhil P George <akhilpgeorge@gmail.com>
2018-02-02 14:28:10 +0530

    removes dist dir

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const os_1 = require("os");
-var numCPUs = os_1.cpus().length;
-console.log(numCPUs);
-//# sourceMappingURL=app.js.map
